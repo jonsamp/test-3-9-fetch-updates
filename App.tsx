56b41dd3c2cc67bdaaae15a53381f774e7151647
Jon Samp <sampjon@gmail.com>
2022-03-09 16:31:41 -0500

    set up for eas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, AppState, AppStateStatus } from 'react-native';
+import * as Updates from 'expo-updates';
 
 export default function App() {
+  const fetchExpoUpdate = async () => {
+    if (!__DEV__) {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+
+        if (update.isAvailable) {
+          alert('Found an update that is avaiable, going to fetch and reload.');
+          await Updates.fetchUpdateAsync();
+          Updates.reloadAsync();
+        }
+      } catch (error) {
+        alert(`Error fetching latest Expo update: ${error}`);
+      }
+    }
+  };
+
+  const handleAppStateChange = (state: AppStateStatus) => {
+    if (state === 'active') {
+      fetchExpoUpdate();
+    }
+  };
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Is __DEV__ true? {__DEV__ ? 'yes' : 'no'}</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
